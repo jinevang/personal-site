8eb049067c7e8012c93ef8a52c5affc25f39e7f3
jinevang <jinevang@gmail.com>
2024-10-30 15:37:38 -0700

    make resume cards a bit wider to fit larger screens

--- src/pages/ResumePage.tsx
+++ src/pages/ResumePage.tsx
@@ -13,7 +13,7 @@ import { FaGithub } from 'react-icons/fa';
 const ResumeCard = styled(Box)({
   borderRadius: '5px',
   padding: '3ch 4ch',
-  maxWidth: '75ch',
+  maxWidth: '60vw',
   boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
   '& p': {
     fontFamily: 'inherit',
